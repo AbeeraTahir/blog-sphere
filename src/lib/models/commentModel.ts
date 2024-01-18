ff81520a58a7d01c2a14dfeb363345e114680408
Abeera Tahir <abeeratahir2018@gmail.com>
2024-01-18 18:46:22 +0500

    feat(comments): add comments schema

---
+++ src/lib/models/commentModel.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+  {
+    content: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+  },
+  { timestamps: true }
+);
+
+const Comment =
+  mongoose.models.comments || mongoose.model("Comment", commentSchema);
+
+export default Comment;
