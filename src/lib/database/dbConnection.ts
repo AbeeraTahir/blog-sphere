c78b7c38e02fd84cd98bd81b32af980b1b074711
Abeera Tahir <abeeratahir2018@gmail.com>
2024-01-18 20:48:11 +0500

    fix: Update dbConnection.ts

--- src/lib/database/dbConnection.ts
+++ src/lib/database/dbConnection.ts
@@ -1,40 +1,19 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
-
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
-
 export const connect = async () => {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose.connection;
-    });
-  }
-
   try {
-    cached.conn = await cached.promise;
-  } catch (e) {
-    cached.promise = null;
-    throw e;
-  }
+    await mongoose.connect(process.env.MONGO_URI!);
+    const connection = mongoose.connection;
 
-  return cached.conn;
+    connection.on("connected", () => {
+      console.log("connected");
+    });
+
+    connection.on("error", () => {
+      console.log("connection error");
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Something went wrong", error);
+  }
 };
