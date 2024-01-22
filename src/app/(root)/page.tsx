9798f83009fbfc0b4edd6b1072a36201bf1ceaef
Abeera Tahir <abeeratahir2018@gmail.com>
2024-01-23 00:26:41 +0500

    feat(homepage): add hero section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,11 +1,26 @@
 "use client";
 
-import axios from "axios";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="mx-auto w-full max-w-screen-xl">
+      <div className="h-screen flex justify-center items-center text-center flex-col gap-6 px-5 md:px-20 pt-20 sm:pt-0">
+        <h1 className="font-extrabold text-3xl sm:text-5xl">
+          Connect Through Stories
+        </h1>
+        <h2 className="text-lg sm:text-xl text-muted-foreground">
+          Engage with narratives that inspire, resonate, and spark conversation.
+          Join a community where stories fuel dialogue and inspiration ignites
+          connection
+        </h2>
+        <div className="flex sm:flex-row flex-col items-center gap-4 mt-3">
+          <Button variant={"outline"} className="w-36">
+            Read Stories
+          </Button>
+          <Button className="w-36">Start Writing</Button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -56,50 +56,52 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="flex justify-between px-20 py-6 items-center shadow-sm">
-      <div className="text-[1.5rem] font-bold">BlogSphere</div>
-      <div className="flex items-center gap-8 h-8">
-        {navLinks.map((link) => (
-          <Link href={link.link} key={link.title}>
-            {link.title}
-          </Link>
-        ))}
-        <div className="border h-full" />
-        {user ? (
-          <div className="flex gap-2 relative">
-            <h2>{user.full_name}</h2>
-            <ChevronDown
-              size={18}
-              strokeWidth={1.25}
-              className={`mt-[0.3rem] cursor-pointer transition-transform transform duration-300 ease-in-out ${
-                isDropDownOpen ? "rotate-180" : ""
-              }`}
-              onClick={() => setIsDropDownOpen(!isDropDownOpen)}
-            />
-            {isDropDownOpen && (
-              <div className="absolute top-7 w-32 p-4 bg-[#f8f8f8] rounded-md shadow-md flex flex-col gap-3">
-                <p>My posts</p>
-                <p>Write post</p>
-                <p className="cursor-pointer" onClick={handleLogout}>
-                  Logout
-                </p>
-              </div>
-            )}
-          </div>
-        ) : (
-          <div className="flex gap-5 items-center">
-            <Link href="/login">
-              <Button variant={"outline"} className="w-20">
-                Login
-              </Button>
+    <header className="w-full fixed top-0 z-10">
+      <nav className="flex justify-between px-20 py-6 items-center shadow-sm">
+        <div className="text-[1.5rem] font-bold">BlogSphere</div>
+        <div className="flex items-center gap-8 h-8">
+          {navLinks.map((link) => (
+            <Link href={link.link} key={link.title}>
+              {link.title}
             </Link>
-            <Link href="/signup">
-              <Button className="w-20">Signup</Button>
-            </Link>
-          </div>
-        )}
-      </div>
-    </nav>
+          ))}
+          <div className="border h-full" />
+          {user ? (
+            <div className="flex gap-2 relative">
+              <h2>{user.full_name}</h2>
+              <ChevronDown
+                size={18}
+                strokeWidth={1.25}
+                className={`mt-[0.3rem] cursor-pointer transition-transform transform duration-300 ease-in-out ${
+                  isDropDownOpen ? "rotate-180" : ""
+                }`}
+                onClick={() => setIsDropDownOpen(!isDropDownOpen)}
+              />
+              {isDropDownOpen && (
+                <div className="absolute top-7 w-32 p-4 bg-[#f8f8f8] rounded-md shadow-md flex flex-col gap-3">
+                  <p>My posts</p>
+                  <p>Write post</p>
+                  <p className="cursor-pointer" onClick={handleLogout}>
+                    Logout
+                  </p>
+                </div>
+              )}
+            </div>
+          ) : (
+            <div className="flex gap-5 items-center">
+              <Link href="/login">
+                <Button variant={"outline"} className="w-20">
+                  Login
+                </Button>
+              </Link>
+              <Link href="/signup">
+                <Button className="w-20">Signup</Button>
+              </Link>
+            </div>
+          )}
+        </div>
+      </nav>
+    </header>
   );
 };
 
