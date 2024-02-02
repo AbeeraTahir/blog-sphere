1f5a27d6ef3ca7662afc1672c4a677f7f7bb93a3
Abeera Tahir <abeeratahir2018@gmail.com>
2024-02-02 16:05:11 +0500

    chore: update post list to be a client component

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,47 +1,67 @@
-import React from "react";
+"use client";
+
+import React, { useState, useEffect } from "react";
 import PostCard from "./PostCard";
 import { PostCardProps } from "@/lib/utils";
+import axios from "axios";
 
 interface PostsListProps {
   simplified?: boolean;
   author?: string;
 }
 
-const getPosts = async () => {
-  const res = await fetch("https://blog-sphere-one.vercel.app/api/posts", {
-    cache: "no-store",
-  });
+const PostsList = ({ simplified, author }: PostsListProps) => {
+  // const posts = await getPosts();
+  // let displayedPosts = [];
+  // if (author) {
+  //   displayedPosts = posts?.posts?.filter(
+  //     (post: { author: string }) => post.author === author
+  //   );
+  // } else {
+  //   displayedPosts = posts?.posts || [];
+  // }
+  const [posts, setPosts] = useState<PostCardProps[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
-
-  return res.json();
-};
-
-const PostsList = async ({ simplified, author }: PostsListProps) => {
-  const posts = await getPosts();
-  let displayedPosts = [];
-  if (author) {
-    displayedPosts = posts?.posts?.filter(
-      (post: { author: string }) => post.author === author
-    );
-  } else {
-    displayedPosts = posts?.posts || [];
-  }
+  useEffect(() => {
+    const getPosts = async () => {
+      setIsLoading(true);
+      try {
+        const res = await axios.get("/api/posts");
+        console.log(res.data.posts);
+        setPosts(res.data.posts);
+        if (author) {
+          setPosts(
+            res.data.posts.filter(
+              (post: { author: string }) => post.author === author
+            )
+          );
+        } else {
+          setPosts(res.data.posts);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    void getPosts();
+  }, [author]);
   return (
     <div className="grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center md:place-items-start">
-      {displayedPosts.map((post: PostCardProps) => (
-        <PostCard
-          key={post._id}
-          _id={post._id}
-          title={post.title}
-          content={post.content}
-          image={post.image}
-          author={post.author}
-          category={post.category}
-        />
-      ))}
+      {isLoading
+        ? "loading"
+        : posts?.map((post) => (
+            <PostCard
+              key={post._id}
+              _id={post._id}
+              title={post.title}
+              content={post.content}
+              image={post.image}
+              author={post.author}
+              category={post.category}
+            />
+          ))}
     </div>
   );
 };
