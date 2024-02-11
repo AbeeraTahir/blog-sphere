10d0b6f150dd8ea783c57a2ae74bf748d94e698d
Abeera Tahir <abeeratahir2018@gmail.com>
2024-02-11 13:29:18 +0500

    feat(enhancement): add homepage buttons in login and signup forms

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -23,6 +23,7 @@ import {
   LoginValidator,
 } from "@/lib/validators/authValidator";
 import Link from "next/link";
+import { LogIn, Undo2 } from "lucide-react";
 
 const Login = () => {
   const {
@@ -100,8 +101,19 @@ const Login = () => {
             )}
           </div>
         </CardContent>
-        <CardFooter>
-          <Button className="w-full">
+        <CardFooter className="flex justify-between">
+          <Link href="/">
+            <Button variant="outline" className="w-24 flex gap-3">
+              <span>
+                <Undo2 size={18} strokeWidth={2} />
+              </span>
+              <span>Home</span>
+            </Button>
+          </Link>
+          <Button className="w-24 flex gap-3 items-center">
+            <span>
+              <LogIn size={18} strokeWidth={2} />
+            </span>
             {isLoading ? (
               <ReloadIcon className="h-4 w-4 animate-spin" />
             ) : (
