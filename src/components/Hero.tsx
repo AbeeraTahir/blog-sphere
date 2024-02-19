27b6b54b713dc3acd4368800296a8ac0b9ba8dcd
Abeera Tahir <abeeratahir2018@gmail.com>
2024-02-19 20:09:07 +0500

    chore(enhancements): update hero section text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,12 +12,12 @@ const Hero = () => {
   return (
     <div className="h-screen flex justify-center items-center text-center flex-col gap-6 mt-[-7rem] sm:mt-[-7rem] px-2">
       <h1 className="font-extrabold text-3xl sm:text-5xl">
-        Connect Through Stories
+        Explore and Create Stories on BlogSphere
       </h1>
       <h2 className="text-lg sm:text-xl text-muted-foreground">
-        Engage with narratives that inspire, resonate, and spark conversation.
-        Join a community where stories fuel dialogue and inspiration ignites
-        connection
+        Immerse Yourself in a Diverse Community of Writers and Readers, and
+        Unleash Your Creative Potential. Embark on a Journey of Discovery and
+        Inspiration.
       </h2>
       <div className="flex md:flex-row flex-col gap-3 items-center">
         <Link href="/posts">
@@ -25,7 +25,7 @@ const Hero = () => {
             <span>
               <Newspaper size={18} strokeWidth={1.25} />
             </span>
-            <span>Read Stories</span>
+            <span>Explore Stories</span>
           </Button>
         </Link>
         <Link href={user ? "/posts/createPost" : "/login"}>
